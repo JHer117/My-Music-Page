5baf27556c5233e997318f11c40d6656d0032c32
Ye Olde Laptop <whiteintheiris@gmail.com>
2020-03-13 21:25:58 -0700

    added flask

--- controller_functions.py
+++ controller_functions.py
@@ -1,13 +1,17 @@
-from flask import render_template, redirect, request, session, flash
-from config import bcrypt #,db
+from flask import render_template, redirect, request#, session, flash
+# from config import bcrypt, db
 # from models import Users, Bands
 import re   
 
 
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+# EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 
 # Home Page
 def landing():
     
-    return render_template("index.html")
+    return render_template("index.htm")
+
+def about_me():
+    return render_template("about_me.html")
+
