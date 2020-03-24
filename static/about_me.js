5d244af1864ccc634f3e21f93e2186f0678a498b
Snacks <whiteintheiris@gmail.com>
2020-03-23 23:19:08 -0700

    changed about me to scrolling with bg img still need to pic good bg img

--- static/about_me.js
+++ static/about_me.js
@@ -4,4 +4,22 @@ $(document).ready(function() {
     }).on("mouseout", function() {
         $(this).css("text-decoration","none");
     });
+
+    window.onscroll = function() {stickyNav()};
+    
+    var nav = document.getElementById("box");
+    
+    var sticky = nav.offsetTop;
+    
+    
+    function stickyNav() {
+      if (window.pageYOffset >= sticky) {
+        nav.classList.add("sticky")
+      } else {
+        nav.classList.remove("sticky");
+      }
+    } 
+
+
 });
+
