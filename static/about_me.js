3858262b9772588facef23fd30e80ef2d4f6d846
Snacks <whiteintheiris@gmail.com>
2020-03-28 21:34:59 -0700

    trying to find the right about me section

--- static/about_me.js
+++ static/about_me.js
@@ -1,24 +1,35 @@
 $(document).ready(function() {
-    $("#aboutme_nav_link #soundcloud_nav_link #youtube_nav_link #contactme_nav_link").on("mousover", function() {
-        $(this).css("text-decoration","line-through");
-    }).on("mouseout", function() {
-        $(this).css("text-decoration","none");
-    });
 
-    window.onscroll = function() {stickyNav()};
+    // $("#aboutme_nav_link #soundcloud_nav_link #youtube_nav_link #contactme_nav_link").on("mousover", function() {
+    //     $(this).css("text-decoration","line-through");
+    // }).on("mouseout", function() {
+    //     $(this).css("text-decoration","none");
+    // });
+
+
+
+    // function toggle(){
+    //     let curtain = document.getElementById("curtain");
+    //     curtain.classList.toggle("active")
+
+    }
+
+
+
+    // window.onscroll = function() {stickyNav()};
     
-    var nav = document.getElementById("box");
+    // var nav = document.getElementById("box");
     
-    var sticky = nav.offsetTop;
+    // var sticky = nav.offsetTop;
     
     
-    function stickyNav() {
-      if (window.pageYOffset >= sticky) {
-        nav.classList.add("sticky")
-      } else {
-        nav.classList.remove("sticky");
-      }
-    } 
+    // function stickyNav() {
+    //   if (window.pageYOffset >= sticky) {
+    //     nav.classList.add("sticky")
+    //   } else {
+    //     nav.classList.remove("sticky");
+    //   }
+    // } 
 
 
 });
