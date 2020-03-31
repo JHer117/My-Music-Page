fac3e31a6747182d29e65e496a7d31659092c492
Snacks <whiteintheiris@gmail.com>
2020-03-30 21:33:05 -0700

    cleaned up js file

--- static/about_me.js
+++ static/about_me.js
@@ -1,35 +1,6 @@
 $(document).ready(function() {
 
-    // $("#aboutme_nav_link #soundcloud_nav_link #youtube_nav_link #contactme_nav_link").on("mousover", function() {
-    //     $(this).css("text-decoration","line-through");
-    // }).on("mouseout", function() {
-    //     $(this).css("text-decoration","none");
-    // });
-
-
-
-    // function toggle(){
-    //     let curtain = document.getElementById("curtain");
-    //     curtain.classList.toggle("active")
-
-    // }
-
-
-
-    // window.onscroll = function() {stickyNav()};
-    
-    // var nav = document.getElementById("box");
-    
-    // var sticky = nav.offsetTop;
-    
-    
-    // function stickyNav() {
-    //   if (window.pageYOffset >= sticky) {
-    //     nav.classList.add("sticky")
-    //   } else {
-    //     nav.classList.remove("sticky");
-    //   }
-    // } 
+   
 
 
 });
